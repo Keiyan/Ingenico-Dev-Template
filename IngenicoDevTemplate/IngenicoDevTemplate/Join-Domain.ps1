6f42aad6152a690c20a2496f739168559302b7fb
Mick Philippon <Mick.philippon@cellenza.com>
2017-02-03 14:26:20 +0100

    Should work now

--- IngenicoDevTemplate/IngenicoDevTemplate/Join-Domain.ps1
+++ IngenicoDevTemplate/IngenicoDevTemplate/Join-Domain.ps1
@@ -3,12 +3,12 @@
 #
 Param(
     [string] [Parameter(Mandatory=$true)] $DomainUserName,
-    [SecureString] $DomainUserPassword
+    [String] $DomainUserPassword
 )
 
 $ip = (Test-Connection -ComputerName "devsharepointad" -Count 1).IPV4Address.IPAddressToString
 $password = ConvertTo-SecureString $DomainUserPassword -AsPlainText -Force 
-$domainCrendentials = New-Object System.Management.Automation.PSCredential ($DomainUserName, $password)
+$domainCrendentials = New-Object System.Management.Automation.PSCredential ("dev.ingenico.azure\$DomainUserName", $password)
 
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ($ip)
 Add-Computer -DomainName "dev.ingenico.azure" -Credential $domainCrendentials -Restart
