73ba967f9616011dd0cf68ed659297572916a386
Mick Philippon <Mick.philippon@cellenza.com>
2017-02-03 11:37:48 +0100

    Guess what ? Syntax error !

--- IngenicoDevTemplate/IngenicoDevTemplate/Join-Domain.ps1
+++ IngenicoDevTemplate/IngenicoDevTemplate/Join-Domain.ps1
@@ -2,12 +2,12 @@
 # Join_Domain.ps1
 #
 Param(
-    [string] [Parameter(Mandatory=$true)] $DomainJoinUserName,
-    [SecureString] $DomainJoinUserPassword
+    [string] [Parameter(Mandatory=$true)] $DomainUserName,
+    [SecureString] $DomainUserPassword
 )
 
 $ip = (Test-Connection -ComputerName "devsharepointad" -Count 1).IPV4Address.IPAddressToString
-$domainCrendentials = New-Object System.Management.Automation.PSCredential ($DomainJoinUserName, $DomainJoinUserPassword)
+$domainCrendentials = New-Object System.Management.Automation.PSCredential ($DomainUserName, $DomainUserPassword)
 
 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ($ip)
 Add-Computer -DomainName "dev.ingenico.azure" -Credential $domainCrendentials -Restart
